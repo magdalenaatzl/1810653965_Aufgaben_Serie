36e8b4830cc3a34fe8e48192e8086b241f0ea37f
magdalenaatzl <1810653965@stud.fh-kufstein.ac.at>
2018-11-08 15:06:20 +0100

    sixth commit

--- src/drittespackage/Main.java
+++ src/drittespackage/Main.java
@@ -16,8 +16,15 @@ public class Main {
         final String FH_KUFSTEIN_TIROL = "Fachhochschule Kufstein Tirol" ; //wurde auf final gesetzt
         System.out.println(FH_KUFSTEIN_TIROL);
 
-        final String bezeichner;
+        final String BEZEICHNER;
 
         ; //STRG+ALT+L rückt wieder zurück
+
+        System.out.println("Java verwendet für Strings \"doppelte Anführungszeich\" und einzelne Anführungszeichen wie \'c\' um einzelne Zeichen zu definieren");
+        System.out.println("Durch \\ kann mann auch den Backslash selbst beliebig oft ausgeben:\\\\\\");
+        System.out.println("Ich verwende den \b Backspace, die Newline \n und den \t Tabulator");
+        System.out.println("Durch den Wagenrücklauf wird die Zeile zurückgesetzt und der Teil nach dem \r überschreibt die bestehende Zeile. \f ist in der Konsole nicht bemerkbar, da es einen Seitenumbruch erzwingt");
+
+
     }
 }
