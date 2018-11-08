07dfda724c7b4739991dc0395cb6782785237a33
magdalenaatzl <1810653965@stud.fh-kufstein.ac.at>
2018-11-08 13:55:52 +0100

    third commit

--- src/org/launchcode/Main.java
+++ src/org/launchcode/Main.java
@@ -1,13 +1,16 @@
-package org.launchcode;
+package org.launchcode; //Erstellung erstes packages
 
 import java.sql.SQLOutput;
 
-public class Main {
+public class Main { //Erstellung einer Klasse
 
-    public static void main(String[] args)
+    public static void main(String[] args) //Erstellung der Main-Methode
     {
-        System.out.println("Hello World");
-        System.out.println ("Magdalena Atzl");
+        System.out.println("Hello World"); //Hello World wird in der Konsole ausgegeben
+        System.out.println ("Magdalena Atzl"); //Mein Name wird in der Konsole ausgegeben
         System.out.println("Hallo");
-    }
-}
+        /*ich habe den Shortcut sout+tab getestet
+        und Hallo in der Konsole ausgegeben
+         */
+    } //Ende der Main-Methode
+}//Ende der Klasse "Main"
--- src/zweitespackage/NeueKlasse.java
+++ src/zweitespackage/NeueKlasse.java
@@ -1,8 +1,8 @@
-package zweitespackage;
+package zweitespackage; //Erstellung eines Package
 
-public class NeueKlasse {
-    public static void main(String []args)
+public class NeueKlasse { //Erstellung einer Klasse
+    public static void main(String []args) //Start der Main-Methode
     {
-        System.out.println("FH Kufstein Tirol");
-    }
-}
+        System.out.println("FH Kufstein Tirol"); //FH Kufstein Tirol wird in der Konsole ausgegeben
+    }//Ende der Main Methode
+}//Ende der Main Klasse
