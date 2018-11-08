f991aada169bb216177a400c253dd7f6906fb841
magdalenaatzl <1810653965@stud.fh-kufstein.ac.at>
2018-11-08 13:45:59 +0100

    second commit

--- src/org/launchcode/Main.java
+++ src/org/launchcode/Main.java
@@ -4,7 +4,10 @@ import java.sql.SQLOutput;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello World");
+        System.out.println ("Magdalena Atzl");
+        System.out.println("Hallo");
     }
 }
---
+++ src/zweitespackage/NeueKlasse.java
@@ -0,0 +1,8 @@
+package zweitespackage;
+
+public class NeueKlasse {
+    public static void main(String []args)
+    {
+        System.out.println("FH Kufstein Tirol");
+    }
+}
