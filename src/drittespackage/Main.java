3d3878f7d7182e64edabe21efca4444674afa2e9
magdalenaatzl <1810653965@stud.fh-kufstein.ac.at>
2018-11-08 14:10:42 +0100

    fourth commit

---
+++ src/drittespackage/Main.java
@@ -0,0 +1,12 @@
+package drittespackage;
+
+public class Main {
+    public static void main(String[]args)
+    {
+        String name = "Magdalena Atzl";
+        int alter = 21;
+
+        System.out.println(name);
+        System.out.println(alter);
+    }
+}
